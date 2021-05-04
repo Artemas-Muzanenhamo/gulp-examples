53d0c30dad91b0c3ef4bbab54ff75e6d234c6212
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-05-04 22:13:37 +0100

    add example of bad gulp practice

---
+++ example-1/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const runSequence = require('gulp4-run-sequence');
+const files = {
+    htmlPath: 'app/*.html'
+}
+
+gulp.task('serve', (done) => {
+    gulp.src(files.htmlPath)
+        .pipe(gulp.dest('dist'));
+    done();
+});
+
+gulp.task('clean', (done) => {
+    gulp.dest('dist')
+        .pipe(clean());
+    done();
+});
+
+gulp.task('default', gulp.series('clean', 'serve'));
